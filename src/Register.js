ab0d83a4b9f3a176fc66fc29165210672df8f58a
AliTahir10 <74157964+AliTahir10@users.noreply.github.com>
2023-03-31 16:00:01 +0100

    Added Register Form

--- src/Register.js
+++ src/Register.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import "./Register.css";
 import "./Home.css";
+import { Button } from "@material-ui/core";
 
 function Register() {
   return (
@@ -8,9 +9,42 @@ function Register() {
     <div className="register">
       <div className="register__header">
         <h2>Register</h2>
+
+        <form action="action_page.php"/>
+  <div class="container">
+    <p>Please fill in this form to create an account.</p>
+    <hr/>
+
+    <label for="email"><b>Email</b></label>
+    <input type="text" placeholder="Enter Email" name="email" id="email" required/>
+
+    <label for="psw"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="psw" id="psw" required/>
+
+    <label for="psw-repeat"><b>Repeat Password</b></label>
+    <input type="password" placeholder="Repeat Password" name="psw-repeat" id="psw-repeat" required/>
+    <hr/>
+
+    {/* <p>By creating an account you agree to our <a href="#">Terms & Privacy</a>.</p> */}
+    {/* <button type="submit" class="registerbtn">Register</button> */}
+    <Button
+          variant="outlined"
+          className="sidebar__tweet"
+          fullWidth
+          input type="submit"
+        >
+          Register
+        </Button>
+  </div>
+
+  <div class="container signin">
+    <p>Already have an account? <a href="/login">Sign in</a>.</p>
+  </div>
+<form/>
       </div>
       </div>
       </div>
+      
   )
 }
 
