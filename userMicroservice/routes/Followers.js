ff42e59d9287c84621750a7194f2560c2cc27c09
Tamim Almahdi <talmahdi.2181@gmail.com>
2023-04-18 03:15:55 +0100

    Add getFollowings call

--- userMicroservice/routes/Followers.js
+++ userMicroservice/routes/Followers.js
@@ -29,16 +29,41 @@ router.post("/follow", validateToken, async (req, res) => {
 
 router.post("/unfollow", validateToken, async (req, res) => {
   const { user, follower } = req.body;
-  const mainUser = await Users.findOne({ where: { username: user } });
-  const followerUser = await Users.findOne({ where: { username: follower } });
-  if (followerUser == null || mainUser == null) {
-    res.json({ error: "User or users do not exist" });
+  const userCount = await Users.count({ where: { username: user } });
+  const followerCount = await Users.count({ where: { username: follower } });
+  if (userCount == 0 || followerCount == 0) {
+    res.json({ error: "User or users does not exist" });
   } else {
-    await Followers.create({
-      user: mainUser.id,
-      follower: follower.id,
+    const mainUser = await Users.findOne({ where: { username: user } });
+    const followerUser = await Users.findOne({ where: { username: follower } });
+    const following = await Followers.findOne({
+      where: { user: `${mainUser.id}`, follower: `${followerUser.id}` },
     });
-    res.json("Success, follower removed");
+    if (following != null) {
+      await Followers.destroy({
+        where: { id: following.id },
+      });
+      res.json("Successfully unfollowed");
+    } else {
+      res.json("No such following exists");
+    }
+  }
+});
+
+router.post("/getFollowings", validateToken, async (req, res) => {
+  const { username } = req.body;
+  const user = await Users.findOne({ where: { username: username } });
+  if (user == null) {
+    res.json({ error: "User or users does not exist" });
+  } else {
+    if (followers != null) {
+      const followers = await Followers.findAll({
+        where: { user: `${user.id}` },
+      });
+      res.json(followers);
+    } else {
+      res.json({ error: "No followers" });
+    }
   }
 });
 
