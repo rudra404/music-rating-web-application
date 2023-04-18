2b233ae90e2a2af17db7a39580f74760ab3fe89c
jamie <jamesprestwich93@gmail.com>
2023-04-18 15:30:34 +0100

    Moved user ratings from Profile to MyRates page

--- front-end/src/MyRates.js
+++ front-end/src/MyRates.js
@@ -1,17 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState, useContext } from "react";
 import "./MyRates.css";
 import "./Home.css";
+import axios from "axios";
+import { AuthContext } from "./helpers/AuthContext";
 
 function MyRates() {
+  const { userID } = useContext(AuthContext);
+  const [allRatings, setAllRatings] = useState({ ratings: [] });
+
+  async function getRatings() {
+    const response = await axios.get(
+      `http://localhost:5000/all_ratings/?userID=${userID}`
+    );
+    const ratings = response.data || [];
+    const updatedRatings = [];
+    for (let i = 0; i < ratings.length; i++) {
+      const id = ratings[i][0];
+      const rating = ratings[i][1];
+      const songInfoResponse = await axios.get(
+        `http://localhost:5000/search_id/${id}`
+      );
+      const songInfo = songInfoResponse.data[0];
+      updatedRatings.push([songInfo[1], songInfo[2], songInfo[3], rating]);
+    }
+    setAllRatings({
+      ratings: updatedRatings,
+    });
+  }
+  useEffect(() => {
+    getRatings();
+  });
   return (
     <div className="home">
-    <div className="myrates">
-      <div className="myrates__header">
-        <h2>My Rates</h2>
+      <div className="profile">
+        <div className="profile__header">
+          <h2>Profile</h2>
+        </div>
+        <div className="profile__ratings">
+          <h3>Ratings:</h3>
+          <ul>
+            {allRatings.ratings.map((rating, index) => (
+              <li key={index}>
+                {rating[0]} - {rating[1]} - {rating[2]} | Rating: {rating[3]}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-      </div>
-      </div>
-  )
+    </div>
+  );
 }
 
-export default MyRates
+export default MyRates;
--- front-end/src/Profile.js
+++ front-end/src/Profile.js
@@ -6,28 +6,7 @@ import ProfileCard from "./ProfileCard";
 
 function Profile() {
   const { userID } = useContext(AuthContext);
-  const [allRatings, setAllRatings] = useState({ ratings: [] });
   const [followers, setFollowers] = useState([]);
-
-  async function getRatings() {
-    const response = await axios.get(
-      `http://localhost:5000/all_ratings/?userID=${userID}`
-    );
-    const ratings = response.data || [];
-    const updatedRatings = [];
-    for (let i = 0; i < ratings.length; i++) {
-      const id = ratings[i][0];
-      const rating = ratings[i][1];
-      const songInfoResponse = await axios.get(
-        `http://localhost:5000/search_id/${id}`
-      );
-      const songInfo = songInfoResponse.data[0];
-      updatedRatings.push([songInfo[1], songInfo[2], songInfo[3], rating]);
-    }
-    setAllRatings({
-      ratings: updatedRatings,
-    });
-  }
 
   async function getFollowers() {
     const data = { userID: userID };
@@ -47,7 +26,6 @@ function Profile() {
   }
 
   useEffect(() => {
-    getRatings();
     getFollowers();
   });
 
@@ -59,17 +37,6 @@ function Profile() {
         </div>
 
         <ProfileCard username={"tamim1"} followers={followers} />
-
-        <div className="profile__ratings">
-          <h3>Ratings:</h3>
-          <ul>
-            {allRatings.ratings.map((rating, index) => (
-              <li key={index}>
-                {rating[0]} - {rating[1]} - {rating[2]} | Rating: {rating[3]}
-              </li>
-            ))}
-          </ul>
-        </div>
       </div>
     </div>
   );
