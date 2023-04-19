51da22d7bafcdfbd96bb004ac471d6828861493b
Tamim Almahdi <talmahdi.2181@gmail.com>
2023-04-19 16:48:02 +0100

    Add feedMicroservice

---
+++ feedMicroservice/app.py
@@ -0,0 +1,43 @@
+from flask import Flask, request
+from flask_cors import CORS 
+import requests
+import json
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def hello_world():
+   return 'musicMicroservice is running on port 5051'
+
+@app.route('/getFeed', methods=['POST', 'GET'])
+def getFeed():
+   if request.method == 'GET':
+       userID = request.args.get('userID')
+       accessToken = request.headers.get('accessToken')
+       accessToken = str(accessToken)
+       print(accessToken)
+
+       headers = {"accessToken": accessToken}
+       data = {
+            "userID": userID,
+        }
+       followers = requests.post('http://localhost:3002/followings/getFollowings', 
+                                 json=data, 
+                                 headers=headers) 
+       followers = followers.json()
+       print(followers)
+       
+       followersID = []
+        for i in range (len(followers)):
+            followersID.append(followers[i]['followerID'])
+            print(followersID)
+
+   # userID
+
+   # get users' followings' ratings 
+   # collate into one array sorted by recency 
+   # return array
+   return 'done'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=5051)
--- front-end/src/Home.js
+++ front-end/src/Home.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Feed from "./Feed";
 import Widgets from "./Widgets";
 import "./Home.css";
+import { AuthContext } from "./helpers/AuthContext";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
+  const navigate = useNavigate();
+  const { userID, authState } = useContext(AuthContext);
+  const [feed, setFeed] = useState([]);
+
+  useEffect(() => {}, [userID]);
+
   return (
     <div className="home">
       <Feed className="feed" />
