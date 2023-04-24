5e66ab43562ec9c565a8224e0bef844a57ab261f
Tamim Almahdi <talmahdi.2181@gmail.com>
2023-04-24 23:26:57 +0100

    Css for ratings page - Ali

--- front-end/src/MyRates.js
+++ front-end/src/MyRates.js
@@ -4,6 +4,7 @@ import "./Home.css";
 import Widgets from "./Widgets";
 import axios from "axios";
 import { AuthContext } from "./helpers/AuthContext";
+import { Link } from "react-router-dom";
 import Header from "./Header";
 
 function MyRates() {
@@ -33,6 +34,18 @@ function MyRates() {
     getRatings();
   }, [userID]);
 
+  function ListSong({ result }) {
+    return (
+      <>
+        <div className="songtitle">
+          <h4>{result[0]}</h4>
+        </div>
+        <div className="artist">By {result[1]}</div>
+        <div className="album">{result[2]}</div>
+      </>
+    );
+  }
+
   return (
     <div className="home">
       <Header className="main_header" />
@@ -41,14 +54,30 @@ function MyRates() {
           <h2>My rates</h2>
         </div>
         <div className="profile__ratings">
-          <h3>Ratings:</h3>
-          <ul>
-            {allRatings.ratings.map((rating, index) => (
-              <li key={index}>
-                {rating[0]} - {rating[1]} - {rating[2]} | Rating: {rating[3]}
-              </li>
-            ))}
-          </ul>
+          <div className="ratingHeadings">
+            <h3>Songs</h3>
+            <h3>Ratings</h3>
+          </div>
+
+          {allRatings.ratings.map((rating, index) => (
+            <div className="listRatings">
+              <Link
+                to={`/song/${rating[0]}`}
+                key={index}
+                className="search-result-links"
+              >
+                <ListSong result={rating} />
+                {/* <SearchResultItem
+                        result={song}
+                        className="search-result-items"
+                      /> */}
+              </Link>
+              <div className="rating">{rating[3]}</div>
+            </div>
+            // <li key={index}>
+            //   {rating[0]} - {rating[1]} - {rating[2]} | Rating: {rating[3]}
+            // </li>
+          ))}
         </div>
       </div>
       <Widgets className="widgets" />
